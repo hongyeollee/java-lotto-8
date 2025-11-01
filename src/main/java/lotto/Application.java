af0090d2ccf334c7fdc1d42236fbf657ff36fef1
이홍열 <zeler1004@gmail.com>
2025-11-02 00:54:41 +0900

    feat(application): add main function for lotto process

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.generateNumbers.GenerateRandomNumbers;
+import lotto.serivce.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoService lottoService = new LottoService(new GenerateRandomNumbers());
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+        try {
+            lottoController.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
